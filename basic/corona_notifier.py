3d644a0bebe7a33c7f3aa8aa76de72a3d20499ff
@erenJan <erencanerel@gmail.com>
2021-01-24 13:18:55 +0300

    corona_notifier

--- basic/corona_notifier.py
+++ basic/corona_notifier.py
@@ -1,14 +1,15 @@
 import requests
 import json
+import os
 import time 
-import pync
+from plyer import notification
 
 #here is the free link of api https://covid-193.p.rapidapi.com/statistics
 url = "https://covid-193.p.rapidapi.com/statistics"
 #here is the rapi api key and host section
 #sign up rapiapi and get ur api key for free
 headers = {
-    'x-rapidapi-key': "60ab51196bmsh7870bbd7d4ac2c2p1f4a6ajsnffd540113285",
+    'x-rapidapi-key': "write your api key here",
     'x-rapidapi-host': "covid-193.p.rapidapi.com"
     }
 #write the country which wants to know about it 
@@ -18,15 +19,31 @@ querystring = {
 #it return json formatted response
 response = requests.request("GET", url, headers=headers, params = querystring)
 response = json.loads(response.text)
-
 date = response['response'][0]['day']
 active = response['response'][0]['cases']['active']
 recovered = response['response'][0]['cases']['recovered']
-new = response['response'][0]['deaths']['new']
+new = response['response'][0]['cases']['new']
+today_deaths = response['response'][0]['deaths']['new']
+total_deaths = response['response'][0]['deaths']['total']
+img_path = os.path.join("resources", "corona.ico")
 
 while(True):
-        notification(
-            "dlkfsdfklsdşfsdkl"
-        )
-        time.sleep(60)
+    notification.notify(
+        #title of the notification,
+        title = "COVID19 Stats",
+        #the body of the notification
+        message = "Today cases : {new_case}\nToday deaths : {new_deaths}\nActive cases : {active}\nTotal deaths : {total_deaths}\nTotal recovered : {recovered}".format(
+            new_case = new,
+            new_deaths = today_deaths,
+            active = active,
+            total_deaths = total_deaths,
+            recovered = recovered
+        ),  
+        #creating icon for the notification
+        #we need to download a icon of ico file format
+        app_icon = img_path,
+        # the notification stays for 50sec
+        timeout  = 60
+    )
+    time.sleep(60*60*12)
 
